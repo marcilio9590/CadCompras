d7d9423d1fbbaa8ec420ac0abf6fc24ad5ab04b4
marcilio9590 <marcilio9590@gmail.com>
2018-06-16 20:37:41 -0300

    telas retiradas poupança;

--- app/scripts/controllers/list_retirada_poupanca.js
+++ app/scripts/controllers/list_retirada_poupanca.js
@@ -8,10 +8,28 @@
  * Controller of the gestDividasApp
  */
 angular.module('gestDividasApp')
-  .controller('ListRetiradaPoupancaCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ListRetiradaPoupancaCtrl', function ($scope, loginService) {
+
+    var vm = $scope;
+    vm.retiradas = [];
+    vm.goTo = loginService.goTo;
+
+    function listarRetiradas() {
+      var date = new Date();
+      var dia = date.getDate();
+      for (var i = 0; i < 10; i++) {
+        vm.retiradas.push({
+          codigo: i,
+          valor: i + "000",
+          dia: dia + i,
+          motivo: "Motivo " + i
+        });
+      }
+    }
+
+    function activate() {
+      listarRetiradas();
+    }
+    activate();
+
   });
