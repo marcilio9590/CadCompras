3689e472bf9fb12b88c9e9d2564a45ce5ec04db9
marcilio9590 <marcilio9590@github.com>
2018-06-17 14:51:26 -0300

    estatisticas tela;

--- app/scripts/controllers/estatisticas.js
+++ app/scripts/controllers/estatisticas.js
@@ -8,10 +8,34 @@
  * Controller of the gestDividasApp
  */
 angular.module('gestDividasApp')
-  .controller('EstatisticasCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('EstatisticasCtrl', function ($scope, loginService) {
+    var vm = $scope;
+    vm.goTo = loginService.goTo;
+    vm.creditoSalario = [];
+    vm.responsaveis = [];
+    vm.forms = {};
+
+    function obterDiasCreditoSalarios() {
+      vm.creditoSalario.push({ codigo: 1, valor: 615, dia: 17 }, { codigo: 2, valor: 765, dia: 28 });
+    }
+
+    function obterResponsaveis() {
+      vm.responsaveis.push({ codigo: 1, nome: "Junior" }, { codigo: 2, nome: "Fatima" });
+    }
+
+    function obterEstatisticaPorDiaResp() {
+      //TODO:
+    }
+
+    vm.limpar = function () {
+      vm.forms = {};
+    }
+
+    function activate() {
+      obterDiasCreditoSalarios();
+      obterResponsaveis();
+    }
+
+    activate();
+
   });
